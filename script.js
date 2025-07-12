17baa3bf28daee3e889eba1e1ff08ca7662a7eb5
Renato Aranda <renato.c.aranda@gmail.com>
2025-07-11 21:06:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ function color(valor) {
 
   var number = document.getElementById("lastNumber");
   var item = document.getElementById(valor);
+  var last-numbers = []
   if (item.style.color != 'white') {
     item.style.backgroundColor = 'black';
     item.style.color = 'white';
     if (valor != "linha" && valor != "cartela") {
-      number.innerHTML = valor;
+      if(last-numbers.length <= 5)
+      {
+        last-numbers.pop();
+        last-numbers.splice(0,0,valor);
+      }
+      number.innerHTML = last-numbers;
     }
   }
   else {
